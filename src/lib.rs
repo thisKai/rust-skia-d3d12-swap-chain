2ecef9bada9fd0e6e110ea770bb0a6c39aa84b01
Kai Patrick <thiskaiguy@gmail.com>
2024-06-05 19:54:57 +0100

    Export HwndSwapChain

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ mod d3d12;
 #[cfg(feature = "windows-ui-composition")]
 mod windows_ui_composition;
 
-pub use d3d12::{swap_chain::SwapChain, Backend};
+pub use d3d12::{
+    swap_chain::{HwndSwapChain, SwapChain},
+    Backend,
+};
 
 #[cfg(feature = "windows-ui-composition")]
 pub use windows_ui_composition::{CompositionBackend, CompositionSwapChain, CompositionTarget};
