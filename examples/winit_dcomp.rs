b05eab31fe60e7da46e2fc47a69ad076a3a685b2
Kai Patrick <thiskaiguy@gmail.com>
2024-06-06 17:59:39 +0100

    Resizing DComp swapchain (WIP)

--- examples/winit_dcomp.rs
+++ examples/winit_dcomp.rs
@@ -1,5 +1,6 @@
 use skia_d3d12_swap_chain::DCompBackend;
 use skia_safe::{colors, Canvas, Paint};
+use windows::Win32::Graphics::DirectComposition::DCOMPOSITION_BACKFACE_VISIBILITY_HIDDEN;
 use winit::{
     dpi::PhysicalSize,
     event::{Event, WindowEvent},
@@ -21,22 +22,27 @@ fn main() {
 
     let target = dcomp.create_target_for_window(&window).unwrap();
 
-    let size = window.inner_size();
+    let mut size = window.inner_size();
     let mut swap_chain = dcomp.create_swap_chain(size.width, size.height).unwrap();
     swap_chain
         .draw(&mut dcomp, |canvas| draw(canvas, size))
         .unwrap();
 
-    unsafe {
+    let visual = unsafe {
         let visual = dcomp.dcomp_desktop_device.CreateVisual().unwrap();
         visual
             .SetContent(swap_chain.unwrap_inner_swap_chain())
+            .unwrap();
+        visual
+            .SetBackFaceVisibility(DCOMPOSITION_BACKFACE_VISIBILITY_HIDDEN)
             .unwrap();
 
         target.SetRoot(&visual).unwrap();
 
         dcomp.dcomp_desktop_device.Commit().unwrap();
-    }
+
+        visual
+    };
 
     event_loop.set_control_flow(ControlFlow::Wait);
 
@@ -53,11 +59,32 @@ fn main() {
             Event::WindowEvent {
                 event: WindowEvent::RedrawRequested,
                 ..
-            } => {}
+            } => {
+                swap_chain
+                    .draw(&mut dcomp, |canvas| draw(canvas, size))
+                    .unwrap();
+            }
             Event::WindowEvent {
                 event: WindowEvent::Resized(new_size),
                 ..
-            } => {}
+            } => {
+                swap_chain.resize(&mut dcomp, new_size.width, new_size.height);
+                size = new_size;
+
+                if let Some(inner_swap_chain) = swap_chain.new_inner_swap_chain(&mut dcomp).unwrap()
+                {
+                    unsafe {
+                        visual.SetContent(inner_swap_chain).unwrap();
+                    }
+                }
+                swap_chain
+                    .draw(&mut dcomp, |canvas| draw(canvas, size))
+                    .unwrap();
+
+                unsafe {
+                    dcomp.dcomp_desktop_device.Commit().unwrap();
+                }
+            }
             _ => (),
         })
         .unwrap();
